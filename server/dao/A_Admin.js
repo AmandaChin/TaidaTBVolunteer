597c10a084e05cc216efb784afed4157664d59ce
Lance CHEN <946304724@qq.com>
2018-01-26 15:49:09 +0800

    完成了 A_Admin、Administrator、AllUser、B_Admin、OtherUser 的dao层

--- server/dao/A_Admin.js
+++ server/dao/A_Admin.js
@@ -1,11 +1,30 @@
-const A_AdminManage = {
-  // A级管理员
-  A_Admin: {
-    addA_Admin: 'insert into A_Admin(UserID) values (?)'
-    // deleteAllUserByAccount: 'delete from AllUser where Account = ?',
-    // updateAllUser: 'insert into user(id, name, age) values (0, ?, ?)',
-    // searchAllUser: 'insert into user(id, name, age) values (0, ?, ?)',
-  }
+const a_admin = require('../util/ormSequelize').A_Admin
+
+function insertA_Admin(UserID) {
+  a_admin.create({
+    'UserID': UserID
+  }).then(function(result) {
+    console.log('inserted A_Admin ok')
+    console.log(result.message)
+  }).catch(function(err) {
+    console.log('inserted A_Admin error')
+    console.log(err.message)
+  })
 }
 
-module.exports = A_AdminManage
+function selectA_AdminByUserID(UserID, count) {
+  a_admin.findAndCountAll({
+    where: {
+      'UserID': UserID
+    }
+  }).then(function(result) {
+    count = result.count
+    return count
+  }).catch(function(err) {
+    console.log('selectA_AdminByUserID error')
+    console.log(err.message)
+  })
+}
+
+exports.insertA_Admin = insertA_Admin
+exports.selectA_AdminByUserID = selectA_AdminByUserID
--- server/dao/OtherUser.js
+++ server/dao/OtherUser.js
@@ -1,12 +1,80 @@
-const OtherUserManage = {
-  // 除超级管理员之外的其它用户
-  OtherUser: {
-    addOtherUser: 'insert into OtherUser(UserID, UserName, Gender, Photo, ' +
-    'Name, IDNumber, Email, Phone, Address) values (?, ?, ?, ?, ?, ?, ?, ?, ?)'
-    // deleteAllUserByAccount: 'delete from AllUser where Account = ?',
-    // updateAllUser: 'insert into user(id, name, age) values (0, ?, ?)',
-    // searchAllUser: 'insert into user(id, name, age) values (0, ?, ?)',
-  }
+const otherUser = require('../util/ormSequelize').OtherUser
+
+function insertOtherUser(UserID, UserName, Gender, Photo, Name,
+  IDNumber, Email, Phone, Address) {
+  otherUser.create({
+    'UserID': UserID,
+    'UserName': UserName,
+    'Gender': Gender,
+    'Photo': Photo,
+    'Name': Name,
+    'IDNumber': IDNumber,
+    'Email': Email,
+    'Phone': Phone,
+    'Address': Address
+  }).then(function(result) {
+    console.log('insertOtherUser ok')
+    console.log(result.message)
+  }).catch(function(err) {
+    console.log('insertOtherUser error')
+    console.log(err.message)
+  })
 }
 
-module.exports = OtherUserManage
+function selectOtherUserByUserID(UserID, ReturnList) {
+  otherUser.findAndCountAll({
+    where: {
+      'UserID': UserID
+    }
+  }).then(function(result) {
+    var list = []
+    for (var i = 0; i < result.count; i++) {
+      list.push(result.rows[i])
+    }
+    return ReturnList(list)
+  }).catch(function(err) {
+    console.log('selectAdministrator error')
+    console.log(err.message)
+  })
+}
+
+function updateOtherUserByUserID(UserID, UserName, Gender, Photo, Name,
+  IDNumber, Email, Phone, Address) {
+  otherUser.findAndCountAll({
+    where: {
+      'UserID': UserID
+    }
+  }).then(
+    function(result) {
+      if (result.count === 0) {
+        console.log('this authority is not exist.')
+      } else {
+        otherUser.update({
+          'UserName': UserName,
+          'Gender': Gender,
+          'Photo': Photo,
+          'Name': Name,
+          'IDNumber': IDNumber,
+          'Email': Email,
+          'Phone': Phone,
+          'Address': Address
+        }, {
+          where: {
+            'UserID': UserID
+          }
+        }
+        ).then(function(result) {
+          console.log('updateOtherUser ok')
+          console.log(result.message)
+        }).catch(function(err) {
+          console.log('updateOtherUser error')
+          console.log(err.message)
+        })
+      }
+    }
+  )
+}
+
+exports.insertOtherUser = insertOtherUser
+exports.selectOtherUserByUserID = selectOtherUserByUserID
+exports.updateOtherUserByUserID = updateOtherUserByUserID
