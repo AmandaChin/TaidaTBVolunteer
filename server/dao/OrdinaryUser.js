9a287132adb7db18b7f373559908afbfa8d93d55
Lance CHEN <946304724@qq.com>
2018-01-26 22:06:19 +0800

    完成了 Application、Demand、OrdinaryUser、Service、SuperAdmin 的dao层

--- server/dao/OrdinaryUser.js
+++ server/dao/OrdinaryUser.js
@@ -1,12 +1,63 @@
-const OrdinaryUserManage = {
-  // 普通用户
-  OrdinaryUser: {
-    addOrdinaryUser: 'insert into OrdinaryUser(UserID, Duration ' +
-    'ServedDuration) values (?, ?, ?)'
-    // deleteAllUserByAccount: 'delete from AllUser where Account = ?',
-    // updateAllUser: 'insert into user(id, name, age) values (0, ?, ?)',
-    // searchAllUser: 'insert into user(id, name, age) values (0, ?, ?)',
-  }
+const ordinaryUser = require('../util/ormSequelize').OrdinaryUser
+
+function insertOrdinaryUser(UserID, Duration, ServedDuration) {
+  ordinaryUser.create({
+    'UserID': UserID,
+    'Duration': Duration,
+    'ServedDuration': ServedDuration
+  }).then(function(result) {
+    console.log('insertOrdinaryUser ok')
+    console.log(result.message)
+  }).catch(function(err) {
+    console.log('insertOrdinaryUser error')
+    console.log(err.message)
+  })
 }
 
-module.exports = OrdinaryUserManage
+function selectOrdinaryUserByUserID(UserID, count) {
+  ordinaryUser.findAndCountAll({
+    where: {
+      'UserID': UserID
+    }
+  }).then(function(result) {
+    count = result.count
+    return count
+  }).catch(function(err) {
+    console.log('selectOrdinaryUserByUserID error')
+    console.log(err.message)
+  })
+}
+
+function updateOrdinaryUser(UserID, Duration, ServedDuration) {
+  ordinaryUser.findAndCountAll({
+    where: {
+      'UserID': UserID
+    }
+  }).then(
+    function(result) {
+      if (result.count === 0) {
+        console.log('this OrdinaryUser is not exist.')
+      } else {
+        ordinaryUser.update({
+          'Duration': Duration,
+          'ServedDuration': ServedDuration
+        }, {
+          where: {
+            'UserID': UserID
+          }
+        }
+        ).then(function(result) {
+          console.log('updateOrdinaryUser ok')
+          console.log(result.message)
+        }).catch(function(err) {
+          console.log('updateOrdinaryUser error')
+          console.log(err.message)
+        })
+      }
+    }
+  )
+}
+
+exports.insertOrdinaryUser = insertOrdinaryUser
+exports.selectOrdinaryUserByUserID = selectOrdinaryUserByUserID
+exports.updateOrdinaryUser = updateOrdinaryUser
