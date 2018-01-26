9df9c80fd66b598c85854bd5efd1ff427c76188f
fuyutong <1776599544@qq.com>
2018-01-26 15:17:24 +0800

    修改了router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// const _import = require('./_import_' + process.env.NODE_ENV)
+const _import = require('./_import_' + process.env.NODE_ENV)
 // in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
 // detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
+
 Vue.use(Router)
-import Volunteers from '../views/program/oldman'
-export default new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: [
-    {
-      path: '/',
-      component: Volunteers
-    }
-  ]
-})
-
-export const constantRouterMap = [
-  {
-    path: '/',
-    component: Volunteers,
-    redirect: 'dashboard'
-  }
-]
-
-export const asyncRouterMap = []
 
 /* Layout */
-// import Layout from '../views/layout/Layout'
+import Layout from '../views/layout/Layout'
+
 /** note: submenu only apppear when children.length>=1
  *   detail see  https://panjiachen.github.io/vue-element-admin-site/#/router-and-nav?id=sidebar
  **/
 
 /**
  * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+ *                                if not set alwaysShow, only more than one route under the children
+ *                                it will becomes nested mode, otherwise not show the root menu
  * redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
  * name:'router-name'             the name is used by <keep-alive> (must set!!!)
  * meta : {
@@ -43,7 +27,7 @@ export const asyncRouterMap = []
     noCache: true                if fasle ,the page will no be cached(default is false)
   }
  **/
-/* export const constantRouterMap = [
+export const constantRouterMap = [
   { path: '/login', component: _import('login/index'), hidden: true },
   { path: '/authredirect', component: _import('login/authredirect'), hidden: true },
   { path: '/404', component: _import('errorPage/404'), hidden: true },
@@ -80,6 +64,17 @@ export default new Router({
 
 export const asyncRouterMap = [
   {
+    path: '/program',
+    component: Layout,
+    children: [{
+      path: 'oldman',
+      name: 'oldman',
+      component:() => import('@/views/program/oldman'),
+      meta: { title:'我是老人',icon:'program'}
+    }]
+  },
+
+  {
     path: '/permission',
     component: Layout,
     redirect: '/permission/index',
@@ -93,18 +88,6 @@ export const asyncRouterMap = [
         icon: 'lock',
         roles: ['admin'] // or you can only set roles in sub nav
       }
-    }]
-  },
-
-  {
-    path: '/zip',
-    component: Layout,
-    redirect: '/zip/download',
-    children: [{ 
-      path: 'download', 
-      component: _import('zip/index'), 
-      name: 'exportZip', 
-      meta: { title: '通知', icon: 'notice' }
     }]
   },
 
@@ -245,6 +228,15 @@ export const asyncRouterMap = [
   },
 
   {
+    path: '/zip',
+    component: Layout,
+    redirect: '/zip/download',
+    alwaysShow: true,
+    meta: { title: 'zip', icon: 'zip' },
+    children: [{ path: 'download', component: _import('zip/index'), name: 'exportZip', meta: { title: 'exportZip' }}]
+  },
+
+  {
     path: '/theme',
     component: Layout,
     redirect: 'noredirect',
@@ -265,4 +257,4 @@ export const asyncRouterMap = [
   },
 
   { path: '*', redirect: '/404', hidden: true }
-] */
+]
