5c408380abcc948f90280566799b98d4688f9850
Lance CHEN <946304724@qq.com>
2018-01-27 10:51:19 +0800

    完成了Dao层的基本部分

--- server/dao/CheckNotice.js
+++ server/dao/CheckNotice.js
@@ -1,12 +1,49 @@
-const CheckNoticeManage = {
-  // 用户检查通知
-  CheckNotice: {
-    addCheckNotice: 'insert into CheckNotice(NoticeID, UserID, Checked)' +
-    ' values (?, ?, ?)'
-    // deleteAllUserByAccount: 'delete from AllUser where Account = ?',
-    // updateAllUser: 'insert into user(id, name, age) values (0, ?, ?)',
-    // searchAllUser: 'insert into user(id, name, age) values (0, ?, ?)',
-  }
+const checkNotice = require('../util/ormSequelize').CheckNotice
+
+function insertCheckNotice(NoticeID, UserID, Checked) {
+  checkNotice.create({
+    'NoticeID': NoticeID,
+    'UserID': UserID,
+    'Checked': Checked
+  }).then(function(result) {
+    console.log('insertCheckNotice ok')
+    console.log(result.message)
+  }).catch(function(err) {
+    console.log('insertCheckNotice error')
+    console.log(err.message)
+  })
 }
 
-module.exports = CheckNoticeManage
+function updateCheckNotice(NoticeID, UserID, Checked) {
+  checkNotice.findAndCountAll({
+    where: {
+      'NoticeID': NoticeID,
+      'UserID': UserID
+    }
+  }).then(
+    function(result) {
+      if (result.count === 0) {
+        console.log('this CheckNotice is not exist.')
+      } else {
+        checkNotice.update({
+          'Checked': Checked
+        }, {
+          where: {
+            'NoticeID': NoticeID,
+            'UserID': UserID
+          }
+        }
+        ).then(function(result) {
+          console.log('updateCheckNotice ok')
+          console.log(result.message)
+        }).catch(function(err) {
+          console.log('updateCheckNotice error')
+          console.log(err.message)
+        })
+      }
+    }
+  )
+}
+
+exports.insertCheckNotice = insertCheckNotice
+exports.updateCheckNotice = updateCheckNotice
