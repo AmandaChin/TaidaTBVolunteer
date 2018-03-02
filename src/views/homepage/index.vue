18efcb69ac29aa4dc9e1a014fbf2965f0d63f6be
XJJ <julietxiao99@qq.com>
2018-03-02 15:37:38 +0800

    首页增加图片轮播组件

--- src/views/homepage/index.vue
+++ src/views/homepage/index.vue
@@ -1,17 +1,39 @@
 <<template>
-  <div class="homepage">
-    <h1>{{msg}}</h1>
-    
-  </div>
+  <slide-show :slides="slides" :inv="slideSpeed"></slide-show>
 </template>
 
-<<script>
+<script>
+import slideShow from '../../components/SlideShow/slideShow'
+
 export default {
-  name: 'homepage',
-  data() {
-    return {
-      msg: '默认主页'
-    }
+  components:{
+      slideShow
+  },
+  data(){
+      return {
+          slides:[
+                { 
+                src:require("./image/1.jpg"),
+                title:'1.jpg'
+                //href:'http://www.baidu.com'
+                },
+                {
+                src:require("./image/2.jpg"),
+                title:'2.jpg'
+                //href:'index'
+                },
+                {
+                src:require("./image/3.jpg"),
+                title:'3.jpg'
+                //href:'./index'
+                },
+                {
+                src:require("./image/4.jpg"),
+                title:'4.jpg'
+                //href:'../form/index'
+                }
+          ]
+      }
   }
 }
 </script>
