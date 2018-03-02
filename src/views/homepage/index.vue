f003f82179168786353f4fcbc0df0bb27cafe4d9
XJJ <julietxiao99@qq.com>
2018-03-02 15:41:49 +0800

    修改了图片描述

--- src/views/homepage/index.vue
+++ src/views/homepage/index.vue
@@ -14,22 +14,22 @@ export default {
           slides:[
                 { 
                 src:require("./image/1.jpg"),
-                title:'1.jpg'
+                title:'欢迎志愿者的加入'
                 //href:'http://www.baidu.com'
                 },
                 {
                 src:require("./image/2.jpg"),
-                title:'2.jpg'
+                title:'像小天使般的志愿者'
                 //href:'index'
                 },
                 {
                 src:require("./image/3.jpg"),
-                title:'3.jpg'
+                title:'志愿服务从今天开始'
                 //href:'./index'
                 },
                 {
                 src:require("./image/4.jpg"),
-                title:'4.jpg'
+                title:'志愿服务从小做起'
                 //href:'../form/index'
                 }
           ]
