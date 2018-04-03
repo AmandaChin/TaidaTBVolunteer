13f2830999fe082d976d98471c888e08afaf213c
XJJ <julietxiao99@qq.com>
2018-04-03 20:55:31 +0800

    搜索界面更新

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,18 +1,318 @@
 <template>
-  <div class="searchTable-container">
+  <div class="app-container calendar-list-container">
     <div class="filter-container">
-
+      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="服务内容" v-model="listQuery.title">
+      </el-input>
+      <el-date-picker clearable style="width: 200px;" 
+        v-model="listQuery.startTime" 
+        class="filter-item" 
+        type="datetime" 
+        placeholder="选择服务开始时间"></el-date-picker>
+      <el-select clearable style="width: 120px" class="filter-item" v-model="listQuery.duration" placeholder="服务时长">
+        <el-option v-for="item in durationOptions" :key="item" :label="item" :value="item">
+        </el-option>
+      </el-select>
+      <el-select clearable class="filter-item" style="width: 130px" v-model="listQuery.type" placeholder="服务地区">
+        <el-option v-for="item in  placeOptions" :key="item.key" :label="item.display_name" :value="item.key">
+        </el-option>
+      </el-select>
+      
+      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">搜索</el-button>
+      
     </div>
-    <el-table>
-        
-    </el-table>
-    <div class="pagination-container">
 
+    <el-table :key='tableKey' :data="list" v-loading="listLoading" element-loading-text="加载中" border fit highlight-current-row
+      style="width: 80%">
+    
+      <el-table-column width="150px" align="center" label="服务日期">
+        <template slot-scope="scope">
+          <span>{{scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}')}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column min-width="150px" label="服务内容">
+        <template slot-scope="scope">
+          <span class="link-type" @click="dialogFormVisible = true">{{scope.row.title}}</span>       
+
+            <el-dialog title="服务详情" :visible.sync="dialogFormVisible">
+          <el-form :rules="rules" ref="dataForm" :model="temp" label-position="left" width="50%" style='width: 400px; margin-left:50px;'>
+            <el-form-item label="服务日期" prop="timestamp">
+              <span>{{scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}')}}</span>
+            </el-form-item>
+
+            <el-form-item label="服务对象" prop="title">
+              <span>{{ scope.row.author }}</span>
+            </el-form-item>
+
+            <el-form-item label="服务内容" prop="title">
+              <span>{{ scope.row.title }}</span>
+            </el-form-item>
+            
+            <el-form-item label="具体事宜" prop="content">
+              <!--<span>{{ scope.row.content }}</span>-->
+              <span>数据库中的备注</span>
+            </el-form-item>
+
+            <el-form-item label="服务时长">
+              <span>{{ scope.row.importance }}</span>
+            </el-form-item>
+
+            <el-form-item label="联系方式">
+              <!--<span>{{ scope.row.phone }}</span>-->
+              <span>此处为发布人联系方式</span>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取消</el-button>
+            <el-button type="primary" @click="updateData">申请</el-button>
+          </div>
+        </el-dialog>
+      </template>
+      </el-table-column>
+      <el-table-column width="110px" align="center" label="发布作者">
+        <template slot-scope="scope">
+          <span>{{scope.row.author}}</span>
+        </template>
+      </el-table-column>
+      
+      <el-table-column width="80px" align="center" label="服务时长">
+        <template slot-scope="scope">
+          <span>{{scope.row.importance}}</span>
+        </template>
+      </el-table-column>
+      
+      <el-table-column align="center" label="申请" width="100" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button type="primary" size="mini" @click="dialogFormVisible = true">申请</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="pagination-container">
+      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="listQuery.page"
+        :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
+      </el-pagination>
     </div>
   </div>
 </template>
+
 <script>
+import { fetchList, fetchPv, createArticle, updateArticle } from '@/api/article'
+import waves from '@/directive/waves' // 水波纹指令
+import { parseTime } from '@/utils'
+import axios from 'axios'
+
+const placeOptions = [
+  { key: '1', display_name: '同区选择' },
+  { key: '2', display_name: '同市选择' },
+  { key: '3', display_name: '同省选择' },
+  { key: '4', display_name: '全国不限' }
+]
+
+// arr to obj ,such as { CN : "China", US : "USA" }
+const calendarTypeKeyValue = placeOptions.reduce((acc, cur) => {
+  acc[cur.key] = cur.display_name
+  return acc
+}, {})
+
 export default {
-  
+  name: 'complexTable',
+  directives: {
+    waves
+  },
+  data() {
+    return {
+      tableKey: 0,
+      list: null,
+      total: null,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 20,
+        duration: undefined,
+        title: undefined,
+        type: undefined,
+        sort: '+id'
+      },
+      durationOptions: [1, 1.5, 2, 2.5, 3, 3.5, 4],
+      placeOptions,
+      sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
+      statusOptions: ['published', 'draft', 'deleted'],
+      showReviewer: false,
+      temp: {
+        id: undefined,
+        duration: 1,
+        remark: '',
+        timestamp: new Date(),
+        title: '',
+        type: '',
+        status: 'published'
+      },
+      dialogFormVisible: false,
+      dialogStatus: '',
+      textMap: {
+        update: 'Edit',
+        create: 'Create'
+      },
+      dialogPvVisible: false,
+      pvData: [],
+      rules: {
+        type: [{ required: true, message: 'type is required', trigger: 'change' }],
+        timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
+        title: [{ required: true, message: 'title is required', trigger: 'blur' }]
+      },
+      downloadLoading: false
+    }
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'info',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    },
+    typeFilter(type) {
+      return calendarTypeKeyValue[type]
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data.total
+        this.listLoading = false
+      })
+      
+    },
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleSizeChange(val) {
+      this.listQuery.limit = val
+      this.getList()
+    },
+    handleCurrentChange(val) {
+      this.listQuery.page = val
+      this.getList()
+    },
+    handleModifyStatus(row, status) {
+      this.$message({
+        message: '操作成功',
+        type: 'success'
+      })
+      row.status = status
+    },
+    resetTemp() {
+      this.temp = {
+        id: undefined,
+        duration: 1,
+        remark: '',
+        timestamp: new Date(),
+        title: '',
+        status: 'published',
+        type: ''
+      }
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    createData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.temp.id = parseInt(Math.random() * 100) + 1024 // mock a id
+          this.temp.author = 'vue-element-admin'
+          createArticle(this.temp).then(() => {
+            this.list.unshift(this.temp)
+            this.dialogFormVisible = false
+            this.$notify({
+              title: '成功',
+              message: '创建成功',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row) // copy obj
+      this.temp.timestamp = new Date(this.temp.timestamp)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const tempData = Object.assign({}, this.temp)
+          tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
+          updateArticle(tempData).then(() => {
+            for (const v of this.list) {
+              if (v.id === this.temp.id) {
+                const index = this.list.indexOf(v)
+                this.list.splice(index, 1, this.temp)
+                break
+              }
+            }
+            this.dialogFormVisible = false
+            this.$notify({
+              title: '成功',
+              message: '更新成功',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleDelete(row) {
+      this.$notify({
+        title: '成功',
+        message: '删除成功',
+        type: 'success',
+        duration: 2000
+      })
+      const index = this.list.indexOf(row)
+      this.list.splice(index, 1)
+    },
+    handleFetchPv(pv) {
+      fetchPv(pv).then(response => {
+        this.pvData = response.data.pvData
+        this.dialogPvVisible = true
+      })
+    },
+    handleDownload() {
+      this.downloadLoading = true
+      import('@/vendor/Export2Excel').then(excel => {
+        const tHeader = ['timestamp', 'title', 'type', 'duration', 'status']
+        const filterVal = ['timestamp', 'title', 'type', 'duration', 'status']
+        const data = this.formatJson(filterVal, this.list)
+        excel.export_json_to_excel(tHeader, data, 'table-list')
+        this.downloadLoading = false
+      })
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => {
+        if (j === 'timestamp') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
+    }
+  }
 }
 </script>
